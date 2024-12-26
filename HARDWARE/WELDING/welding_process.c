132bdc22c1d92717cec64fb5afec9d1ff1ee1a53
HY <huangyouli.scut@gmail.com>
2024-12-26 11:55:07 +0800

    起始温度记录

--- HARDWARE/WELDING/welding_process.c
+++ HARDWARE/WELDING/welding_process.c
@@ -327,8 +327,8 @@ static void Preload()
  */
 static void First_Step()
 {
-	weld_controller->first_step_start_temp = TEMP_GAIN1 * ADC_Value_avg(ADC_Channel_7) + TEMP_GAIN2; // 记录一阶段起始温度
-	if ((page_param->key2 == ION) && (weld_controller->weld_time[1] != 0))							 // ION_IOF==0开PWM
+
+	if ((page_param->key2 == ION) && (weld_controller->weld_time[1] != 0)) // ION_IOF==0开PWM
 	{
 		/*1、一阶段采样开始*/
 		temp_draw_ctrl->first_step_index_start = 0;
@@ -389,7 +389,6 @@ static void First_Step()
 static void Second_Step()
 {
 
-	weld_controller->second_step_start_temp = TEMP_GAIN1 * ADC_Value_avg(ADC_Channel_7) + TEMP_GAIN2;
 	if ((page_param->key2 == ION) && (weld_controller->weld_time[2] != 0))
 	{
 		/*1、进入二阶段采样*/
@@ -457,7 +456,7 @@ static void Second_Step()
  */
 static void Third_Step()
 {
-	weld_controller->third_step_start_temp = TEMP_GAIN1 * ADC_Value_avg(ADC_Channel_7) + TEMP_GAIN2; // 记录三阶段起始温度
+
 	if ((page_param->key2 == ION) && (weld_controller->weld_time[3] != 0))
 	{
 		/*1、三阶段采样开始，坐标记录*/
@@ -617,37 +616,42 @@ static void weld_real_time_ctrl()
 	/*焊接实时控制*/
 	else
 	{
-		Weld_Preparation(); // 变量初始化以及相关初始化工作
+		/*变量初始化以及相关初始化工作*/
+		Weld_Preparation();
 
 		/*预压*/
 		Preload();
-		err_cnt_clear(err_ctrl); // 报错统计值复位
 
 		/*一阶段*/
-		reset_forword_ctrl(weld_controller->pid_ctrl);												// pid控制器复位
-		ctrl_param_config(weld_controller);															// 参数配置
-		pid_param_dynamic_reload(weld_controller, fitting_curves, weld_controller->first_step_set); // kp动态调整
-		weld_controller->pid_ctrl->stable_flag = false;												// 稳态标志复位
-		First_Step();																				// 一阶段
-		if (true == err_occur(err_ctrl))															// 唤醒错误处理线程
+		err_cnt_clear(err_ctrl);																		 // 报错统计值复位
+		reset_forword_ctrl(weld_controller->pid_ctrl);													 // pid控制器复位
+		weld_controller->first_step_start_temp = TEMP_GAIN1 * ADC_Value_avg(ADC_Channel_7) + TEMP_GAIN2; // 起始温度
+		ctrl_param_config(weld_controller);																 // 参数配置
+		pid_param_dynamic_reload(weld_controller, fitting_curves, weld_controller->first_step_set);		 // kp动态调整
+		weld_controller->pid_ctrl->stable_flag = false;													 // 稳态标志复位
+		First_Step();																					 // 一阶段
+		if (true == err_occur(err_ctrl))																 // 唤醒错误处理线程
 			goto STOP_LABEL;
 
 		/*二阶段*/
-		err_cnt_clear(err_ctrl);																	 // 报错统计值复位
-		ctrl_param_config(weld_controller);															 // 参数配置
-		pid_param_dynamic_reload(weld_controller, fitting_curves, weld_controller->second_step_set); // kp动态调整
-		weld_controller->pid_ctrl->stable_flag = false;												 // 稳态标志复位
-		Second_Step();																				 // 二阶段
-		if (true == err_occur(err_ctrl))															 // 唤醒错误处理线程
+		err_cnt_clear(err_ctrl);																		  // 报错统计值复位
+		weld_controller->second_step_start_temp = TEMP_GAIN1 * ADC_Value_avg(ADC_Channel_7) + TEMP_GAIN2; // 起始温度
+		ctrl_param_config(weld_controller);																  // 参数配置
+		pid_param_dynamic_reload(weld_controller, fitting_curves, weld_controller->second_step_set);	  // kp动态调整
+		weld_controller->pid_ctrl->stable_flag = false;													  // 稳态标志复位
+		Second_Step();																					  // 二阶段
+		if (true == err_occur(err_ctrl))																  // 唤醒错误处理线程
 			goto STOP_LABEL;
 
 		/*三阶段*/
-		err_cnt_clear(err_ctrl);		 // 报错统计值复位
-		Third_Step();					 // 三阶段
-		if (true == err_occur(err_ctrl)) // 唤醒错误处理线程
+		err_cnt_clear(err_ctrl);																		 // 报错统计值复位
+		weld_controller->third_step_start_temp = TEMP_GAIN1 * ADC_Value_avg(ADC_Channel_7) + TEMP_GAIN2; // 起始温度
+		Third_Step();																					 // 三阶段
+		if (true == err_occur(err_ctrl))																 // 唤醒错误处理线程
 			goto STOP_LABEL;
 
-		End_of_Weld(); // 结束一轮焊接
+		/*结束一轮焊接*/
+		End_of_Weld();
 	}
 
 STOP_LABEL:
